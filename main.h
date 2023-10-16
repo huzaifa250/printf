fa7141dd912fd2be074f458904fcbac736ebbc55
Tiedaman33 <orinabravin6@gmail.com>
2023-10-16 11:58:51 -0700

    initialcommit

--- main.h
+++ main.h
@@ -3,6 +3,19 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} match;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int printf_char(va_list val);
+int print_string(va_list val);
+int print_37(void);
+int _strlen(char *str);
+int _strlenc(const char *str);
+
 #endif
---
+++ print_str.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strlen - Rturns the legth of a string
+ * @str: string pointer
+ * Return: 1
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+	return (i);
+}
+/**
+ * _strlenc - strlen fucntion
+ * @str: char pointer
+ * Return: i
+ */
+int _strlenc(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+	return (i);
+}
