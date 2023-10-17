99754d512c8e7a7e7bf4a48f436ed836988d458f
huzaifa250 <huz.dark1@gmail.com>
2023-10-17 16:30:22 -0700

    edit betty

--- print_des.c
+++ print_des.c
@@ -15,23 +15,29 @@ int print_dec(va_list args)
 	num = n;
 
 	if (last < 0)
+	{
 		_putchar('-');
 		num = -num;
 		n = -n;
 		last = -last;
 		i++;
+	}
 	if (num > 0)
 	{
 		while (num / 10 != 0)
+		{
 			exp = exp * 10;
 			num = num / 10;
+		}
 		num = n;
 		while (exp > 0)
+		{
 			digit = num / exp;
 			_putchar(digit + '0');
 			num = num - (digit * exp);
 			exp = exp / 10;
 			i++;
+		}
 	}
 	_putchar(last + '0');
 	return (i);
