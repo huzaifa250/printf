6404960f8b8163c314f98ad9202b83f6ca074d5e
Tiedaman33 <orinabravin6@gmail.com>
2023-10-17 03:43:47 -0700

    function that produces output according to a format

--- main.h
+++ main.h
@@ -1,21 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <string.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} match;
-
+#include <stdlib.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
-int printf_char(va_list val);
-int print_string(va_list val);
-int print_37(void);
-int _strlen(char *str);
-int _strlenc(const char *str);
 
 #endif
--- mainprintf.c
+++ mainprintf.c
@@ -7,32 +7,41 @@
  */
 int _printf(const char *format, ...)
 {
+	/* initialize an array match of format specifiers and their associated fucnitons*/
 	match m[] = {{"%c", printf_char}, {"%s", print_string}, {"%%", print_37}};
 	va_list args;
 	int i = 0, len = 0;
 	int j;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	/* returns error if format is NULL or contain %only*/
+	 if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+
+	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
-		j = 13;
+		/* initialize a label for 'goto' statement. to contniue processecing after handling a specifier*/
+Here:
+		j = 2;/* size of array *this size will increase as we handle more task since we will be adding format specifier and thier associated functions*/
 		while (j >= 0)
 		{
+			/*check if the current and next characters match format specifiers in the array*/
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
-				len = len + m[j].f(args);
-				i= i+2;
-				break;
+				/*update the position and length of he correspodning printing functions*/
+				len += m[j].f(args);
+				i += 2;
+				goto Here;
 			}
 			j--;
 		}
+		/*if no mathc found, print the charter the update the position and lenght*/
 		_putchar(format[i]);
 		i++;
 		len++;
 	}
+	/*clean up args list and return number of characters printed*/
 	va_end(args);
 	return (len);
 }
